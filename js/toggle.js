bfee7d18bb3a062f90d6955ba92712bf175935b5
Wilmir <wbnicanor01@gmail.com>
2019-12-12 22:34:15 +0000

    Updates nav bar of all pages

---
+++ js/toggle.js
@@ -0,0 +1,9 @@
+
+
+    <!--Responsive navbar toggle script-->
+
+    let mainNav = document.getElementById('js-menu');
+    let navBarToggle = document.getElementById('js-navbar-toggle');
+    navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active-nav');
+});
